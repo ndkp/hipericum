ee42dda7198af99fff89c8ae3b4f2f6311db17b7
ndkp <nk.we99@gmail.com>
2021-03-11 23:18:31 +0900

    fullcalendar version down

--- JS/schedule.js
+++ JS/schedule.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+
+  $(document).ready(function() {
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,listYear'
+      },
+      displayEventTime: false, 
       googleCalendarApiKey: 'AIzaSyBCSlo5E13JaKg_1T6bNeOVfHXC7DWdG-A',
-      eventSources: [
-        {
-          googleCalendarId: 'nfo.hipericum@gmail.com'
-        }
-      ]
+      events: 'info.hipericum@gmail.com',
+      eventClick: function(event) {
+        // opens events in a popup window
+        window.open(event.url, 'gcalevent', 'width=700,height=600');
+        return false;
+      },
+      loading: function(bool) {
+        $('#loading').toggle(bool);
+      }
     });
-    calendar.render();
   });
