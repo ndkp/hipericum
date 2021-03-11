5ad207242e56f406e31306199090019783d4d5a8
ndkp <nk.we99@gmail.com>
2021-03-11 23:03:14 +0900

    calendar test

--- JS/schedule.js
+++ JS/schedule.js
@@ -1,26 +1,12 @@
-
- document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
-  });
-  calendar.render();
-});
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    let calendarEl = document.getElementById('calendar');
-    let calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'interaction', 'dayGrid', 'list', 'googleCalendar'],
-      locale: 'ja',
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,listYear'
-      },
-      displayEventTime: false, 
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
       googleCalendarApiKey: 'AIzaSyBCSlo5E13JaKg_1T6bNeOVfHXC7DWdG-A',
-      events: 'info.hipericum@gmail.com',
+      eventSources: [
+        {
+          googleCalendarId: 'nfo.hipericum@gmail.com'
+        }
+      ]
     });
     calendar.render();
   });
