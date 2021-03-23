098a6c7145636c2386fb14d36a146dc89364528f
ndkp <nk.we99@gmail.com>
2021-03-23 11:22:50 +0900

    modal:sparkles:

--- JS/discography.js
+++ JS/discography.js
@@ -1,3 +1,14 @@
 $(function(){
+    // モーダル表示
+    $('#js-modal-show01').click(function(){
+        $('#cd-modal01').fadeIn();
+      });
     
-});
+    // 閉じるボタンクリックでモーダル非表示
+      $('.close-modal').click(function(){
+        $('#cd-modal01').fadeOut();
+       // $('#cd-modal02').fadeOut();
+       // $('#cd-modal03').fadeOut();
+      });
+      
+    });
