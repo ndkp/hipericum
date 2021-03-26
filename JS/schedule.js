f6d2538afd0590043e611251ae42666b1097ec62
ndkp <nk.we99@gmail.com>
2021-03-26 17:05:38 +0900

    changed js

--- JS/schedule.js
+++ JS/schedule.js
@@ -1,6 +1,21 @@
 
-       document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,listYear',
+          },
+          googleCalendarApiKey: 'AIzaSyBCSlo5E13JaKg_1T6bNeOVfHXC7DWdG-A',
+          eventSources: [
+            {
+              googleCalendarId: 'info.hipericum@gmail.com'
+            }
+          ]
+        });
+
+        
         var calendar = new FullCalendar.Calendar(calendarEl, {
           googleCalendarApiKey: 'AIzaSyBCSlo5E13JaKg_1T6bNeOVfHXC7DWdG-A',
           eventSources: [
@@ -9,47 +24,10 @@
             }
           ]
         });
+        
         calendar.render();
       });
       
+
+
       
-      $(document).ready(function() {
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,listYear'
-      },
-      displayEventTime: false, 
-      googleCalendarApiKey: 'AIzaSyBCSlo5E13JaKg_1T6bNeOVfHXC7DWdG-A',
-      events: 'info.hipericum@gmail.com',
-      eventClick: function(event) {
-        // opens events in a popup window
-        window.open(event.url, 'gcalevent', 'width=700,height=600');
-        return false;
-      },
-      loading: function(bool) {
-        $('#loading').toggle(bool);
-      }
-    });
-  });
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      firstDay: 1,
-      headerToolbar: {
-        left: "dayGridMonth,listMonth",
-        center: "title",
-        right: "today prev,next"
-      },
-      buttonText: {
-        today: '今月',
-        month: '月',
-        list: 'リスト'
-      },
-    });
-    //キャンバスにレンダリング
-    calendar.render();
-  });
