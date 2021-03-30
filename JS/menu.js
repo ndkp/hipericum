e77974ec4d42facfd5b69d5b980c4be6f4edfa85
ndkp <nk.we99@gmail.com>
2021-03-31 01:04:21 +0900

    🍔

---
+++ JS/menu.js
@@ -0,0 +1,14 @@
+
+$(function() {
+
+    // モーダル表示
+    $('#menu-icon').click(function(){
+        $('#').fadeIn();
+        });
+    
+    // 閉じるボタンクリックでモーダル非表示
+        $('.close-modal').click(function(){
+        $('#').fadeOut();
+    });
+
+});
